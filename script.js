b87037d75d530e7069fcfae0b3dbc199803dd6fc
E-tim <eesuolaayot@gmail.com>
2022-05-01 05:27:02 +0800

    made some changes

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 let navbarLinks = document.getElementsByClassName('navbarLinks')[0];
-let icons = document.getElementById('farBar');
+let faBars = document.querySelector('.farBar');
+let faTimes = document.querySelector('.faTimes')
 
-icons.addEventListener('click', ()=> {
+faBars.addEventListener('click', ()=> {
     navbarLinks.classList.toggle('active')
+    faBars.classList.add('close')
+    faTimes.classList.add('open')
+    
 })
+
+faTimes.addEventListener('click', ()=> {
+    navbarLinks.classList.toggle('active')
+    faTimes.classList.remove('open')
+    faBars.classList.remove('close')
+
+})
+
 
 
 
@@ -12,6 +24,8 @@ let myLi = document.querySelectorAll('.myLi');
 myLi.forEach(element => {
     element.addEventListener('click', ()=> {
         navbarLinks.classList.toggle('active');
+        faTimes.classList.remove('open')
+        faBars.classList.remove('close')
     })
 });
 
@@ -40,18 +54,61 @@ spa.addEventListener('click', ()=> {
 
 // go back up
 
-let scrollup = document.getElementsByClassName('backup')[0];
+// let scrollup = document.getElementsByClassName('backup')[0];
 
-const toTop = () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollup.className = 'backup'
-    } else {
-        scrollup.className = 'onBackup'
-    }
+// const toTop = () => {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         scrollup.className = 'backup'
+//     } else {
+//         scrollup.className = 'onBackup'
+//     }
+// }
+// window.onscroll = toTop();
+
+// scrollup.addEventListener('click', ()=> {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// })
+
+//  onscroll animation 
+
+const aboutMe = document.querySelector('.aboutMe')
+const fadeIn = document.querySelectorAll('.fadeIn')
+
+const options = {
+    threshold: 0,
+    rootMargin: '0px 0px 0px 0px'
 }
-window.onscroll = toTop();
+const aboutMeOpacity = new IntersectionObserver( (entries, aboutMeOpacity)=>{
+    entries.forEach( entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('aboutMeFadein')
+            aboutMeOpacity.unobserve(entry.target)
+        }
+    } )
+},options )
+aboutMeOpacity.observe(aboutMe)
 
-scrollup.addEventListener('click', ()=> {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+
+const fadeInOptions = {
+    threshold: 0.25,
+    rootMargin: '0px 0px -100px 0px'
+}
+
+const fadeInAanimation = new IntersectionObserver( (entries, fadeInAanimation)=>{
+    entries.forEach(entry=> {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('fadeInAppear')
+            fadeInAanimation.unobserve(entry.target)
+        }
+    })
+}, fadeInOptions )
+fadeIn.forEach(fade=> {
+    fadeInAanimation.observe(fade)
 })
+
+
